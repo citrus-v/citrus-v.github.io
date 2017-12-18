f84266027b7744211fdc5115fc610a3d85acba00
citrus-v <982773344@qq.com>
2017-12-18 18:42:19 +0800

    13213

--- index/script.js
+++ index/script.js
@@ -1,16 +1,16 @@
-////文字打字机
-//text = '稍等，Loading...';
-//        i = 0;
-//         function type(){ 
-//            str  = text.substr(0,i);
-//            txt.innerHTML = str + "_";
-//            i++;
-//            if (i>text.length){
-//                txt.style.display="none";
-//                zero.style.display="block";
-//            }
-//          setTimeout("type()",200);
-//         }
+//文字打字机
+text = '稍等，Loading...';
+        i = 0;
+         function type(){ 
+            str  = text.substr(0,i);
+            txt.innerHTML = str + "_";
+            i++;
+            if (i>text.length){
+                txt.style.display="none";
+                zero.style.display="block";
+            }
+          setTimeout("type()",200);
+         }
 var bg1024 = document.querySelector('.bg-1024');
 var list1024 = document.querySelector('.list-1024');
 var hide = true;
